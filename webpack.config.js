f81c91339cdf6a97bd707ab25018132451907fe3
Nathan Winch <nwinch@gmail.com>
2015-09-25 09:51:16 +1000

    Create webpack config

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  context: __dirname + '/src',
+  entry: {
+    html: './index.html',
+    javascript: './index.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader']
+      },
+      {
+        test: /\.html$/,
+        loader: 'file?name=[name].[ext]'
+      }
+    ]
+  },
+  output: {
+    filename: 'index.js',
+    path: __dirname + '/dist'
+  }
+}
