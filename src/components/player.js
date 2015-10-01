d9033068de82553d512628463dec93c7c8719626
Nathan Winch <nwinch@gmail.com>
2015-10-02 09:10:07 +1000

    Refactor for play method

--- src/components/player.js
+++ src/components/player.js
@@ -1,9 +1,32 @@
-import React, { Component, DOM } from 'react';
-
-const { div } = DOM;
+import { Howl } from 'howler/howler.min';
+import React, { Component, PropTypes } from 'react';
 
 export default class Player extends Component {
-  render() {
-    return div(null, 'Hello!');
+  constructor(props) {
+    super(props);
+
+    this.state = {isPlaying: false};
+    this.player = new Howl({src: this.props.tracks});
   }
-}
+
+  render() {
+    return (
+      <div>
+        <a href='#' onClick={this._onPlay.bind(this, this.player)}>
+          {this.state.isPlaying ? 'Stop' : 'Play'}
+        </a>
+      </div>
+    );
+  }
+
+  _onPlay(player, e) {
+    let { isPlaying } = this.state;
+
+    e.preventDefault();
+
+    if (!isPlaying) player.play();
+    else player.stop();
+
+    this.setState({isPlaying: !isPlaying});
+  }
+};
